c0efcd33cfa46ab0d65970278828db27fb75f96c
Ahmed Ramy <Dev.AhmedRamy@gmail.com>
2018-12-03 18:19:25 +0200

    Initial Commit

---
+++ ARSoundPlayer/ARSoundPlayer.h
@@ -0,0 +1,19 @@
+//
+//  ARSoundPlayer.h
+//  ARSoundPlayer
+//
+//  Created by Ahmed Ramy on 12/3/18.
+//  Copyright © 2018 ScaRiLiX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ARSoundPlayer.
+FOUNDATION_EXPORT double ARSoundPlayerVersionNumber;
+
+//! Project version string for ARSoundPlayer.
+FOUNDATION_EXPORT const unsigned char ARSoundPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ARSoundPlayer/PublicHeader.h>
+
+
